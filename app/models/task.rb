54ecd51480ca7b8996196d3399eb33fc20374f04
Patrick Morehead <patrick.a.morehead@gmail.com>
2024-05-18 16:39:50 -0500

    Finished todo list

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  def index
+    @tasks = Task.all
+    render 'tasks/index'
+  end
+
   validates :content, length: { maximum: 200 }, presence: true
 end
